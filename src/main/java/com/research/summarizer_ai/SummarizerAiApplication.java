ca935aca510e8421d48eb103bdb265cb5a5c1e08
sahara <sahara.tijol@gmail.com>
2025-10-02 19:01:28 -0500

    starter files

---
+++ src/main/java/com/research/summarizer_ai/SummarizerAiApplication.java
@@ -0,0 +1,13 @@
+package com.research.summarizer_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummarizerAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummarizerAiApplication.class, args);
+	}
+
+}
