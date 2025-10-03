2f1a4b695b1a7785a4c1d7c980e4379ae669fa42
sahara <sahara.tijol@gmail.com>
2025-10-02 20:43:58 -0500

    prepped request body for prompts

--- src/main/java/com/research/summarizer_ai/SummarizerService.java
+++ src/main/java/com/research/summarizer_ai/SummarizerService.java
@@ -2,12 +2,23 @@ package com.research.summarizer_ai;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class SummarizerService {
     public String processContent(SummarizerRequest request) {
 
         // build the prompt
+        String prompt = buildPrompt(request);
+
         // query the ai model api
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                        Map.of("parts", new Object[] {
+                            Map.of("text", prompt)
+                        })
+                }
+        );
         // parse the response
         // return response
     }
@@ -17,7 +28,20 @@ public class SummarizerService {
 
         switch(request.getOperation()) {
             case "summarize":
-                prompt.append("");
+                prompt.append("Summarize the following highlighted text in clear, concise language. Focus only on the " +
+                        "key points and main ideas. Do not add outside information, and avoid unnecessary details or " +
+                        "repetition. If the text contains multiple ideas, present them in a logical order. Provide the " +
+                        "summary in 3–4 sentences:\n\n");
+                break;
+
+            case "suggest":
+                prompt.append("Based on the following content: suggest related topics and further reading. " +
+                        "Format the response with clear headings and bullet points:\n\n");
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown Operation: " + request.getOperation());
         }
+        prompt.append(request.getContent());
+        return prompt.toString();
     }
 }
